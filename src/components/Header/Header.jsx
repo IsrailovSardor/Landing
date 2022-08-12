99eab7f205d21f84e3967aec4e6dc9317a85150b
Israilov Sardor <Sardor_09062002@mail.ru>
2022-08-12 18:40:01 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./Header.scss";
+import { useTranslation } from "react-i18next";
+import logo from "../../assets/images/logo.png";
+import Language from "../Language/Language";
+import { ReactComponent as ArrowUp } from "../../assets/icons/up_arrow.svg";
+import BurgerMenu from "./BurgerMenu/BurgerMenu";
+const Header = () => {
+  const { t } = useTranslation();
+  const [dropDown, serDropDown] = useState(false);
+  const [burgerMenu, setBurgerMenu] = useState(false);
+
+  const links = [
+    { href: "/" },
+    { href: "/" },
+    { href: "/" },
+    { href: "/" },
+    { href: "/" },
+    { href: "/" },
+  ];
+  return (
+    <div className="header">
+      <div className="container">
+        <img src={logo} alt="logo" />
+        <ul className="nav">
+          {links.map((item, index) => (
+            <li key={index}>
+              <a href={item.href}>{t(`navLinks.${index}`)}</a>
+            </li>
+          ))}
+          <li onClick={() => serDropDown(!dropDown)}>
+            <a href="/#">{t("navLinks.6")}</a>
+            <ArrowUp className={`arrow ${dropDown ? "active_row" : ""}`} />
+          </li>
+          <Language dropDown={dropDown} serDropDown={serDropDown} />
+        </ul>
+        <div className="burgerMenu" onClick={() => setBurgerMenu(!burgerMenu)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        {burgerMenu ? (
+          <BurgerMenu
+            dropDown={dropDown}
+            serDropDown={serDropDown}
+            links={links}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.scss'
+import Previe from '../../components/Previe/Previe';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Previe/>
+        </div>
+    );
+};
+
+export default Home;
